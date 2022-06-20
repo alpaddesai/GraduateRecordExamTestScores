443fad84d032d4c51a387fcfb08962f9d03289db
Alpa Desai <alpa.gupta@gmail.com>
2022-06-20 17:54:16 -0400

    initial commit

--- Test Scores/Window1.xaml.cs
+++ Test Scores/Window1.xaml.cs
@@ -10,25 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Test_Scores
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Window1.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Window1 : Window
     {
-        public MainWindow()
+        public Window1()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Window1 ButtonGMATObject = new Window1();
-            ButtonGMATObject.Show();
         }
     }
 }
